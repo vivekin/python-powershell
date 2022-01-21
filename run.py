2ffba761017a019feac80a3fa71db87c1c3821fd
vivekin <39245544+vivekin@users.noreply.github.com>
2022-01-21 19:31:07 +0530

    Delete run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import sys
-
-try:
-    p = subprocess.Popen(['powershell.exe', 'C:\\Users\\v\\Desktop\\python_automation\\a.ps1',"knname"],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    s=p.communicate()
-    print(s)
-except Exception as e:
-    print("Exception occured",e)
