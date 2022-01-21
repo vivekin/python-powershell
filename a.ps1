86026250733563386a9d373c3d397d9a2ecd16d2
vivekin <39245544+vivekin@users.noreply.github.com>
2022-01-21 19:27:06 +0530

    Add files via upload

---
+++ a.ps1
@@ -0,0 +1,11 @@
+﻿
+param ([Parameter(Mandatory)]$name="dummy")
+#param ($name="dummy")
+
+try{
+Write-Host "hello $name" -BackgroundColor Cyan
+}
+catch{
+Write-Host "error" -BackgroundColor Red
+}
+finally {Write-Host "finally" -BackgroundColor Green}
